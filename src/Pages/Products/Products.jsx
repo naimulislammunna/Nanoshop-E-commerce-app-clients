0405fe841f3830289b80b608760f2cbc7011072f
naimulislammunna <naimulislammunna742@gmail.com>
2025-10-22 12:20:37 +0600

    pagination

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -6,6 +6,7 @@ import Loader from "../../Components/Loader";
 import SearchBar from "./SearchBar";
 import SortBar from "./SortBar";
 import FilterBar from "./FilterBar";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Products = () => {
     const axiosPublic = useAxiosPublic();
@@ -31,6 +32,47 @@ const Products = () => {
         setSort('')
     }
 
+    //Pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = 3;
+
+    const handlePrevious = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    const handleNext = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+
+    const handlePageClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const renderPageNumbers = () => {
+        const pageNumbers = [];
+
+        for (let i = 1; i <= totalPages; i++) {
+            pageNumbers.push(
+                <button
+                    key={i}
+                    onClick={() => handlePageClick(i)}
+                    className={`mx-1 px-4 py-2 text-[0.9rem] sm:text-[1rem] rounded-full transform transition-all duration-300 ${currentPage === i
+                        ? "bg-[#3B9DF8] text-white scale-110 shadow-md"
+                        : "bg-transparent text-blue-600 hover:bg-blue-100"
+                        }`}
+                >
+                    {i}
+                </button>
+            );
+        }
+
+        return pageNumbers;
+    };
+
     if (isLoading) return <Loader />
 
     return (
@@ -46,11 +88,31 @@ const Products = () => {
                         <button onClick={handleReset} className="btn bg-sky-950 text-white border-none w-full mx-auto my-4 hover:bg-primary">Reset</button>
                     </div>
                 </div>
-                <div className="grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 col-span-9 mx-auto">
-                    {
-                        products?.result.length ? <>{products?.result?.map(product => <ProductCard key={product._id} item={product}></ProductCard>)} </> : <div className="min-h-full min-w-full col-span-10 justify-center items-center"><h1 className="text-xl text-center font-bold">No Products Found</h1></div>
-                    }
+                <div className="col-span-9">
+                    <div className="grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mx-auto">
+                        {
+                            products?.result.length ? <>{products?.result?.map(product => <ProductCard key={product._id} item={product}></ProductCard>)} </> : <div className="min-h-full min-w-full col-span-10 justify-center items-center"><h1 className="text-xl text-center font-bold">No Products Found</h1></div>
+                        }
+                    </div>
+                    <div className="flex items-center flex-wrap justify-center mt-8 space-x-1 sm:space-x-2">
+                        <button
+                            onClick={handlePrevious}
+                            disabled={currentPage === 1}
+                            className="mx-1 px-3.5 py-3.5 rounded-full bg-white text-blue-600 hover:bg-blue-100 transition-all duration-300 dark:bg-slate-700 dark:disabled:bg-slate-800 dark:hover:bg-slate-600 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
+                        >
+                            <FaChevronLeft />
+                        </button>
+                        {renderPageNumbers()}
+                        <button
+                            onClick={handleNext}
+                            disabled={currentPage === totalPages}
+                            className="mx-1 px-3.5 py-3.5 rounded-full bg-white text-blue-600 hover:bg-blue-100 transition-all duration-300 dark:bg-slate-700 dark:disabled:bg-slate-800 dark:hover:bg-slate-600 disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center"
+                        >
+                            <FaChevronRight />
+                        </button>
+                    </div>
                 </div>
+
             </div>
 
         </div>
@@ -58,3 +120,4 @@ const Products = () => {
 };
 
 export default Products;
+
